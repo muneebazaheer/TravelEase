516b88b00347155a28c4d62f7d61d867f633ed90
Mahad1090 <i230537@isb.nu.edu.pk>
2025-05-05 22:34:34 +0500

    Travller login form completed

--- TravelEaseForms/Forms/TravellerLogin.Designer.cs
+++ TravelEaseForms/Forms/TravellerLogin.Designer.cs
@@ -34,6 +34,7 @@
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
             this.LoginButton = new System.Windows.Forms.Button();
+            this.RegisterLinkLabel = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // WelcomeLabel
@@ -51,35 +52,40 @@
             // EmailLabel
             // 
             this.EmailLabel.AutoSize = true;
-            this.EmailLabel.Location = new System.Drawing.Point(250, 184);
+            this.EmailLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EmailLabel.Location = new System.Drawing.Point(220, 184);
             this.EmailLabel.Name = "EmailLabel";
-            this.EmailLabel.Size = new System.Drawing.Size(48, 20);
+            this.EmailLabel.Size = new System.Drawing.Size(74, 29);
             this.EmailLabel.TabIndex = 1;
             this.EmailLabel.Text = "Email";
             // 
             // PasswordLabel
             // 
             this.PasswordLabel.AutoSize = true;
-            this.PasswordLabel.Location = new System.Drawing.Point(238, 249);
+            this.PasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordLabel.Location = new System.Drawing.Point(196, 249);
             this.PasswordLabel.Name = "PasswordLabel";
-            this.PasswordLabel.Size = new System.Drawing.Size(78, 20);
+            this.PasswordLabel.Size = new System.Drawing.Size(120, 29);
             this.PasswordLabel.TabIndex = 2;
             this.PasswordLabel.Text = "Password";
             // 
             // maskedTextBox1
             // 
             this.maskedTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.maskedTextBox1.Location = new System.Drawing.Point(322, 185);
+            this.maskedTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(322, 184);
             this.maskedTextBox1.Name = "maskedTextBox1";
-            this.maskedTextBox1.Size = new System.Drawing.Size(310, 19);
+            this.maskedTextBox1.Size = new System.Drawing.Size(310, 25);
             this.maskedTextBox1.TabIndex = 3;
             // 
             // maskedTextBox2
             // 
             this.maskedTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.maskedTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.maskedTextBox2.Location = new System.Drawing.Point(322, 249);
             this.maskedTextBox2.Name = "maskedTextBox2";
-            this.maskedTextBox2.Size = new System.Drawing.Size(310, 19);
+            this.maskedTextBox2.PasswordChar = '*';
+            this.maskedTextBox2.Size = new System.Drawing.Size(310, 25);
             this.maskedTextBox2.TabIndex = 4;
             // 
             // LoginButton
@@ -92,11 +98,26 @@
             this.LoginButton.UseVisualStyleBackColor = true;
             this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
             // 
+            // RegisterLinkLabel
+            // 
+            this.RegisterLinkLabel.ActiveLinkColor = System.Drawing.Color.White;
+            this.RegisterLinkLabel.AutoSize = true;
+            this.RegisterLinkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RegisterLinkLabel.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.RegisterLinkLabel.Location = new System.Drawing.Point(342, 437);
+            this.RegisterLinkLabel.Name = "RegisterLinkLabel";
+            this.RegisterLinkLabel.Size = new System.Drawing.Size(155, 26);
+            this.RegisterLinkLabel.TabIndex = 6;
+            this.RegisterLinkLabel.TabStop = true;
+            this.RegisterLinkLabel.Text = "Register Now";
+            this.RegisterLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.RegisterLinkLabel_LinkClicked);
+            // 
             // TravellerLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(910, 504);
+            this.Controls.Add(this.RegisterLinkLabel);
             this.Controls.Add(this.LoginButton);
             this.Controls.Add(this.PasswordLabel);
             this.Controls.Add(this.EmailLabel);
@@ -119,5 +140,6 @@
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox2;
         private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.LinkLabel RegisterLinkLabel;
     }
 }
--- TravelEaseForms/Forms/TravellerLogin.cs
+++ TravelEaseForms/Forms/TravellerLogin.cs
@@ -25,6 +25,10 @@ namespace TravelEaseForms.Forms
             LoginButton.FlatStyle = FlatStyle.Flat;
             LoginButton.BackColor = ColorTranslator.FromHtml("#99BC85");
             LoginButton.ForeColor = Color.White;
+            LoginButton.Font = new Font(LoginButton.Font.FontFamily, LoginButton.Font.Size, FontStyle.Bold);
+            this.BackColor = ColorTranslator.FromHtml("#FAF1E6");
+            RegisterLinkLabel.LinkColor = ColorTranslator.FromHtml("#99BC85");
+            RegisterLinkLabel.LinkBehavior = LinkBehavior.NeverUnderline;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -38,6 +42,11 @@ namespace TravelEaseForms.Forms
         }
 
         private void TravellerLogin_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegisterLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
 
         }
